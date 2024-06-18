e7c49a5473176e9a4900d508dfbd3478ad294025
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-18 14:40:42 +0530

    Fascinating_Number_Upto_n_Program

---
+++ Python/Python_Day_6/Disarium_Number_Upto_n.py
@@ -0,0 +1,20 @@
+def is_Disarium(num):
+    ori_num=num
+    sum=0
+    for i in range(len(str(num)),0,-1):
+        sum=sum+pow(num%10,i)
+        num=num//10
+    if(sum==ori_num):
+        return True
+    else:
+        return False
+
+
+number=int(input("Enter the upper limit number : "))
+
+for i in range ( 1, number+1):
+    if is_Disarium(i):
+        print(f"The {i} is a Disarium Number!")
+
+else:
+    print("Done!")
