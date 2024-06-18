44493d1b88068c6aed3c519b7a834856afae30f2
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-18 17:29:07 +0530

    Interest_Calculate_Program

---
+++ Python/Python_Day_6/Interest_Calculate.py
@@ -0,0 +1,7 @@
+
+
+Principal=int(input("Enter the Principal Amount: "))
+Rate=float(input("Enter Rate: "))
+Time=int(input("Enter time: "))
+
+print("Simple Interest is : ",(Principal*Rate*Time)/100)
