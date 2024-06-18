651ceb3f7caf01a7054d49f9e054ef56dfa4846e
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-18 16:42:20 +0530

    Anagram_Program

---
+++ Python/Python_Day_6/Anagram.py
@@ -0,0 +1,15 @@
+string1=input("Enter the first string : ")
+string2=input('Enter the second string : ')
+print(string2)
+print(string1)
+print(sorted(string1))
+print(sorted(string2))
+
+if(sorted(string2)==sorted(string1)):
+    print(f"The {string1} and {string2} are Anagram!")
+else:
+    print("These String are not Anagram!")
+
+
+
+
