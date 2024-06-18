1c66beddeb6ed7f0c8b04d8b181c953323a983f8
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-18 17:55:57 +0530

    Area_Of_Rectangle_Program

---
+++ Python/Python_Day_6/Area_Of_Reactangle.py
@@ -0,0 +1,8 @@
+
+
+length=int(input("Enter the length of the rectangle : "))
+breadth=int(input("Enter the breadth of the rectangle : "))
+
+area=length*breadth
+
+print(f"The area of the Rectangle is : {area}")
