be299a7388939e387d0889b87c751c6aee181289
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-18 17:51:55 +0530

    Area_Of_Triangle_Program

---
+++ Python/Python_Day_6/Area_Of_Triangle.py
@@ -0,0 +1,7 @@
+
+base=int(input("Enter the base of the triangle : "))
+height=int(input("Enter the height of the triangle : "))
+
+area=1/2*(base*height)
+
+print(f"The are of the triangle is : {area:.2f}")
