65c307b307b437fb9d5e4b28d65e1f886a5c3dc0
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-20 10:18:10 +0530

    Heap_Sort_Program

---
+++ Python/Python_Day_8/Heap_Sort.py
@@ -0,0 +1,33 @@
+def heapify(arr, n, i):
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left = 2*i + 1
+    right = 2 * i + 2  # right = 2*i + 2
+
+    # If left child is larger than root
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    # If right child is larger than largest so far
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    # If largest is not root
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # Swap
+
+        # Recursively heapify the affected sub-tree
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # Swap
+        heapify(arr, i, 0)
+
+arr = [3,2,1]
+heap_sort(arr)
+
+print("Sorted array is:", arr,len(arr))
