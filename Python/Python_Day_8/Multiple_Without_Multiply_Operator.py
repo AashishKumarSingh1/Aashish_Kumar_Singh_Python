88423d627ee3a820d73bc79f034d3d98e7eb7891
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-20 11:18:14 +0530

    Multiple_Without_Multiply_Operator_Program

---
+++ Python/Python_Day_8/Multiple_Without_Multiply_Operator.py
@@ -0,0 +1,13 @@
+
+
+num1=int(input("Enter the first number : "))
+num2=int(input("Enter the second number : "))
+multiply=0
+# 5 * -3 = -15 
+# 5*3=15
+
+
+
+for i in range(1,num2+1):
+    multiply+=num1
+print(multiply)
