25f2751fcb130023abae3b60ef854993ced8b258
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-22 19:33:24 +0530

    Code_Chef_Audible_Range_Program

---
+++ Python/Python_Day_10/Code_Chef_Audible_Range.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+if t>=1 and t<=10**4:
+    for _ in range(t):
+        x=int(input())
+        if x>=67 and x<=45000:
+            print('yes')
+            pass
+        else:
+            print("no")
+            pass
+        
