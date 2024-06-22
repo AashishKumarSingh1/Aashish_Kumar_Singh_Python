604ff7192d6f840897fd5a71b3e87090f10a70a5
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-06-22 19:42:29 +0530

    Code_Chef_Reach_On_Time_Program

---
+++ Python/Python_Day_10/Code_Chef_Reach_On_Time.py
@@ -0,0 +1,12 @@
+# cook your dish here
+t=int(input())
+if t>=1 and t<=60:
+    for _ in range(t):
+        x=int(input())
+        if x-30>=0:
+            print("yes")
+            pass
+        else:
+            print("no")
+            pass
+        
