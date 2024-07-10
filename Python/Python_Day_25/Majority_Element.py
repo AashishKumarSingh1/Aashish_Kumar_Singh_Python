2fccbe7d1f9f6c956463dc92f2a81cef50570bfd
AashishKumarSingh1 <aashishs.ug23.cs@nitp.ac.in>
2024-07-11 03:47:41 +0530

    Majority_Element_Program

---
+++ Python/Python_Day_25/Majority_Element.py
@@ -0,0 +1,17 @@
+"""
+Boyer-Moore Voting Algorithm Based Problem
+"""
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate
+
